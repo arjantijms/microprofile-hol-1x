55ab8a96df8e64b9af392c8b5e18cecfa8414684
Ivan St. Ivanov <ivan.st.ivanov@gmail.com>
2018-03-13 23:41:56 +0200

    Check JWT role of user performing action

--- solution/subscribers/src/main/java/bg/jug/microprofile/hol/subscribers/Application.java
+++ solution/subscribers/src/main/java/bg/jug/microprofile/hol/subscribers/Application.java
@@ -1,7 +1,10 @@
 package bg.jug.microprofile.hol.subscribers;
 
+import org.eclipse.microprofile.auth.LoginConfig;
+
 import javax.ws.rs.ApplicationPath;
 
-@ApplicationPath("/")
+@ApplicationPath("/subscribers")
+@LoginConfig(authMethod = "MP-JWT", realmName = "MP-HOL-JWT")
 public class Application extends javax.ws.rs.core.Application {
 }
--- solution/subscribers/src/main/java/bg/jug/microprofile/hol/subscribers/SubscribersRepository.java
+++ solution/subscribers/src/main/java/bg/jug/microprofile/hol/subscribers/SubscribersRepository.java
@@ -24,6 +24,7 @@ public class SubscribersRepository {
         return Optional.ofNullable(subscribers.get(email));
     }
 
+    @Metered(name = "Subscriber added")
     public void addSubscriber(Subscriber subscriber) {
         subscribers.put(subscriber.getEmail(), subscriber);
     }
